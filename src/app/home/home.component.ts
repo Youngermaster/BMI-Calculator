3b263386f36cfdbad1e615ddfe977ad9a8501fc9
Youngermaster <jmyoungh@eafit.edu.co>
2019-11-10 13:17:16 -0500

    Logic finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,15 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   clickCounter = 0;
   name = '';
-  fuck = '';
   weight = 0;
   height = 0;
-
-  bmi = this.weight / (this.height);
+  condition = '';
+  bmi = 0;
 
   constructor() { }
 
@@ -24,11 +24,34 @@ export class HomeComponent implements OnInit {
     this.clickCounter++;
   }
 
+  determinateBmi() {
+    this.bmi =  this.weight / (this.height * this.height);
+  }
+
+  determinateBmiLevel() {
+    this.determinateBmi();
+    if (this.bmi <= 18) {
+      this.condition = 'Light weight!';
+    } else if (18 < this.bmi && this.bmi <= 25) {
+      this.condition = 'Normal weight';
+    } else if (25 < this.bmi && this.bmi <= 30) {
+      this.condition = 'Overweight';
+    } else if (30 < this.bmi && this.bmi <= 35) {
+      this.condition = 'Obesity level 1';
+    } else if (35 < this.bmi && this.bmi <= 40) {
+      this.condition = 'Obesity level 2';
+    } else if (this.bmi > 40) {
+      this.condition = 'Obesity level 3';
+    } else {
+      this.condition = 'Try another values';
+    }
+  }
+
   setStyle() {
-    let myStyle = {
+    const myStyle = {
       active: this.clickCounter > 4,
       noActive: this.clickCounter <= 4
-    }
+    };
     return myStyle;
   }
 
